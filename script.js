67ce08554ab9a84e38c6e37866961b6905fbf3cc
Abdulrahman <bhabdulrahaman@gmail.com>
2022-06-25 14:48:15 +0100

    trying a different method to generate square divs

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 "use strict";
-let num = 30;
-let size = num * num;
-function createDiv() {
+let num = 10;
+// let size = num * num;
+function createDiv(col) {
   const container = document.querySelector(".container");
-  for (let i = 0; i < size; i++) {
+
+  for (let i = 0; i < num; i++) {
     const div = document.createElement("div");
     div.classList.add("divs");
     div.addEventListener("click", addBackground);
-
     container.appendChild(div);
   }
 }
 createDiv();
 
+let column = 10;
+function col() {
+  for (let i = 0; i < column; i++) {
+    const dive = document.createElement("div");
+    dive.classList.add("dive");
+    dive.addEventListener("click", addBackground);
+    container.appendChild(dive);
+  }
+}
+col();
 function addBackground() {
   this.classList.toggle("addBackground");
 }
