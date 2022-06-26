37a9e8889dea4c26f684e15b7bc482a2d31ed236
Abdulrahman <bhabdulrahaman@gmail.com>
2022-06-27 00:08:35 +0100

    Done with generating the grid

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 "use strict";
-let num = 10;
-// let size = num * num;
-function createDiv(col) {
+
+let num = 30;
+for (let i = 0; i < num; i++) {
+  let row = createRow();
+  for (let k = 0; k < num; k++) {
+    col(row);
+  }
+}
+
+function createRow() {
   const container = document.querySelector(".container");
-
-  for (let i = 0; i < num; i++) {
-    const div = document.createElement("div");
-    div.classList.add("divs");
-    div.addEventListener("click", addBackground);
-    container.appendChild(div);
-  }
+  const div = document.createElement("div");
+  div.classList.add("divs");
+  container.appendChild(div);
+  return div;
 }
-createDiv();
 
-let column = 10;
-function col() {
-  for (let i = 0; i < column; i++) {
-    const dive = document.createElement("div");
-    dive.classList.add("dive");
-    dive.addEventListener("click", addBackground);
-    container.appendChild(dive);
-  }
+function col(container) {
+  const dive = document.createElement("div");
+  dive.classList.add("dive");
+  dive.addEventListener("click", addBackground);
+  container.appendChild(dive);
 }
-col();
+
 function addBackground() {
   this.classList.toggle("addBackground");
 }
